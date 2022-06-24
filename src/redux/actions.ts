8b7779af8038901353edde07b54d66f0774afee6
Angor78 <angor78@gmail.com>
2022-06-24 10:32:00 +0300

    2. Заполнить экшены и экшенкриэйторы в файле action.ts

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,29 +1,23 @@
 export enum ACTIONS_TYPE {
-    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
-    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
-    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+  CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+  CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+  CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
 }
 
 
-export type ChangeCurrencyFieldType = {
+export type ChangeCurrencyFieldType = ReturnType<typeof ChangeCurrencyFieldAC>;
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string) => {
+  return {type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE, amountOfBYN, amountOfCurrency} as const
 };
 
-// @ts-ignore
-export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string): ChangeCurrencyFieldType => {
+export type ChangeAction = ReturnType<typeof ChangeActionAC>;
+export const ChangeActionAC = (isBuying: boolean) => {
+  return {type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION, isBuying}
 };
 
-export type ChangeAction = {
-};
-
-// @ts-ignore
-export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
-};
-
-export type ChangeCurrentCurrencyType = {
-};
-
-// @ts-ignore
-export const СhangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+export type ChangeCurrentCurrencyType = ReturnType<typeof ChangeCurrentCurrencyAC>;
+export const ChangeCurrentCurrencyAC = (currentCurrency: string) => {
+  return {type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY, currentCurrency}
 };
 
 export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
